a3b6649a6804ec62411aca9cb5a7b3b097bb9465
Victor Moraes <victor.moraesrj@gmail.com>
2023-05-05 19:34:26 -0300

    Favicon and Responsiveness

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -20,30 +20,30 @@ function SearchPage() {
       </div>
       <div className='searchPage_resultsBox'>
         <SearchResult
-                  img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU"
-                  location="Private room in center of London"
-                  title="Stay at this spacious Edwardian House"
-                  description="1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine"
+                  img="https://images.unsplash.com/photo-1613575831056-0acd5da8f085?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                  location="Centro de Lambeth"
+                  title="Apartamento Espaçoso em Londres"
+                  description="6 hospedes · 2 Quartos · 4 Camas · 2 Banheiros· Wifi · Cozinha · Estacionamento Grátis · Maquina de lavar"
                   star={4.73}
-                  price="£30 / night"
+                  price="£30 / Noite"
                   total="£117 total"
               />
         <SearchResult
-                  img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU"
-                  location="Private room in center of London"
-                  title="Stay at this spacious Edwardian House"
-                  description="1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine"
+                  img="https://images.unsplash.com/photo-1613575831056-0acd5da8f085?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                  location="Centro de Lambeth"
+                  title="Apartamento Espaçoso em Londres"
+                  description="6 hospedes · 2 Quartos · 4 Camas · 2 Banheiros· Wifi · Cozinha · Estacionamento Grátis · Maquina de lavar"
                   star={4.73}
-                  price="£30 / night"
+                  price="£30 / Noite"
                   total="£117 total"
               />
         <SearchResult
-                  img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU"
-                  location="Private room in center of London"
-                  title="Stay at this spacious Edwardian House"
-                  description="1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine"
+                  img="https://images.unsplash.com/photo-1613575831056-0acd5da8f085?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                  location="Centro de Lambeth"
+                  title="Apartamento Espaçoso em Londres"
+                  description="6 hospedes · 2 Quartos · 4 Camas · 2 Banheiros· Wifi · Cozinha · Estacionamento Grátis · Maquina de lavar"
                   star={4.73}
-                  price="£30 / night"
+                  price="£30 / Noite"
                   total="£117 total"
               />
       </div>
